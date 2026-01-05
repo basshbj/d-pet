5ad4d1c81d80b9d36f54fa66b8008a84f82db1b3
basshbj <bassamhbj@gmail.com>
2026-01-05 22:31:02 +0900

    Created simple animation

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# d-pet
+# Prototype of D-PET
+
+# Status
+- Created simple animation of a d-egg in the cosole
---
+++ main.py
@@ -0,0 +1,61 @@
+import time
+import sys
+
+sprites =[ 
+    [
+        "                ",
+        "                ",
+        "      ████      ",
+        "     ██   █     ",
+        "    ███  ███    ",
+        "   ███   ████   ",
+        "   █     ████   ",
+        "  █  █    ████  ",
+        "  █  ██    ███  ",
+        "  █  ███     █  ",
+        "  █   ██  ██ █  ",
+        "   ██    ████   ",
+        "    ██   ███    ",
+        "     ██████     ",
+        "                ",
+        "                ",
+    ],
+    [
+        "                ",
+        "                ",
+        "                ",
+        "                ",
+        "      ████      ",
+        "     ██   █     ",
+        "    ███  ███    ",
+        "   ███   ████   ",
+        "   █     ████   ",
+        "  █  █    ████  ",
+        "  █  ██    ███  ",
+        "   ██    ████   ",
+        "    ██   ███    ",
+        "     ██████     ",
+        "                ",
+        "                ",
+    ],
+]
+
+H = len(sprites[0])
+
+def draw(frame):
+    # Move cursor up H lines (to the start of the previous frame)
+    sys.stdout.write(f"\033[{H}A")
+    # Clear each line and redraw
+    for line in frame:
+        sys.stdout.write("\033[2K\r")  # clear line
+        sys.stdout.write(line + "\n")
+    sys.stdout.flush()
+
+# Print once to create space for the sprite (so moving up works)
+print("\n" * H, end="")
+
+# Example loop: redraw the same sprite repeatedly
+for i in range (0, 10):
+    for sprite in sprites:
+        draw(sprite)
+        time.sleep(0.5)
