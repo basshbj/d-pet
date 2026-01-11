3404aaff7ef1ae07a906e0bd4b9335bb6c295ba0
basshbj <bassamhbj@gmail.com>
2026-01-11 23:27:08 +0900

    update sprite sheet data format and improved json loading logic

--- sprite_manager.py
+++ sprite_manager.py
@@ -1,11 +1,12 @@
 import json
+from tools.utils import customJsonDecoder
 
 class SpriteManager():
 
     def __init__(self):
         with open("./sprites.json", "r", encoding="utf-8") as f:
-            self.sheet = json.load(f)
+            self.sheet = json.loads(f.read(), object_hook=customJsonDecoder)
 
     
     def get_egg_sprites(self):
-        return self.sheet["egg"]
+        return self.sheet.stages[0].sprites
